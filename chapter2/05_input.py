b6226068c076ef55ddf0687d92e9ac0c4ebce532
Chhabhaiya Mitkumar Prabhudas <mittchhabhaiya24@gmail.com>
2026-01-10 01:18:21 +0530

    input

---
+++ chapter2/05_input.py
@@ -0,0 +1,7 @@
+a = int(input("Enter a number1: "))
+b = int(input("Enter a number2: "))
+print()
+print("Your number1 is:", a)
+print("Your number2 is:", b)
+
+print("The sum of two numbers is:", a + b) #sum of two numbers
