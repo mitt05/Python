9bf9e8f232c947a3143802fc6e09afa251f5030d
Chhabhaiya Mitkumar Prabhudas <mittchhabhaiya24@gmail.com>
2026-01-10 00:22:34 +0530

    operators

---
+++ chapter2/03_operators.py
@@ -0,0 +1,22 @@
+# Arithmetic Operators
+a = 10
+b = 5
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("Modulus:", a % b)
+print("Exponentiation:", a ** b)
+
+#assignment Operators
+x = 10  
+x += 5  # Equivalent to x = x + 5
+print("After += :", x)
+x -= 3  # Equivalent to x = x - 3
+print("After -= :", x) 
+x *= 2  # Equivalent to x = x * 2
+print("After *= :", x)
+x /= 4  # Equivalent to x = x / 4
+print("After /= :", x)
+x %= 4  # Equivalent to x = x % 4
+print("After %= :", x)
