1719360b8a571523a0bbfa636a2c21ea93600518
Chhabhaiya Mitkumar Prabhudas <mittchhabhaiya24@gmail.com>
2026-01-10 01:32:10 +0530

    pb2

---
+++ chapter2/problem_2.py
@@ -0,0 +1,6 @@
+# Write a python program to find remainder when a number is divided by x.
+
+a=int(input("Enter the number: "))
+x=int(input("Enter the divisor: "))
+remainder=a%x
+print("The remainder is:", remainder) 
