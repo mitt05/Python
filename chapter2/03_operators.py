01dece8714bef792d40d933dea1edb418acbe7eb
Chhabhaiya Mitkumar Prabhudas <mittchhabhaiya24@gmail.com>
2026-01-10 00:44:40 +0530

    modified

--- chapter2/03_operators.py
+++ chapter2/03_operators.py
@@ -3,17 +3,11 @@ a = 10
 b = 5
 
 print("Addition:", a + b) #15
-
 print("Subtraction:", a - b) #5
-
 print("Multiplication:", a * b) #50
-
 print("Division:", a / b) #2.0
-
 print("Modulus:", a % b) #0
-
 print("Floor Division:", a // b) #2
-
 print("Exponentiation:", a ** b) #100000
 
 #assignment Operators
